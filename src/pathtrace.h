bb58d835963c8d9ee41615454746a2e159eae558
Sthiber <sthiber133guevara@gmail.com>
2025-05-03 23:32:07 -0500

    Added changes to pathtrace.h, setting up to make optimization changes for src files

--- src/pathtrace.h
+++ src/pathtrace.h
@@ -3,7 +3,17 @@
 #include <vector>
 #include "scene.h"
 
+typedef unsigned char uchar4[4];
+typedef uchar4* uchar4pbo;
+
 void InitDataContainer(GuiDataContainer* guiData);
-void pathtraceInit(Scene *scene);
+void pathtraceInit(Scene scene);
 void pathtraceFree();
-void pathtrace(uchar4 *pbo, int frame, int iteration);
+void pathtrace(uchar4pbo, int frame, int iteration);
+
+// Group3 Mod - Added timing variable declarations for performance measurement
+extern cudaEvent_t startPathTrace, stopPathTrace;
+extern cudaEvent_t startIntersection, stopIntersection;
+extern cudaEvent_t startShading, stopShading;
+extern cudaEvent_t startStreamCompaction, stopStreamCompaction;
+extern float totalTime, intersectionTime, shadingTime, streamCompactionTime;
